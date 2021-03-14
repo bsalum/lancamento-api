bd590a6a859f7c08ee7264dd0e1bc50a8fa1309c
bsalum <bsalum@gmail.com>
2021-03-13 22:48:44 -0300

    iniciando autenticacao

--- src/main/java/com/lancamento/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/lancamento/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,11 +1,12 @@
 package com.lancamento.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.lancamento.model.Lancamento;
 import com.lancamento.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 }
